21955f5f0569a5dadae46fe351c2069d5941cd6e
Bridget McErlean <bridgetmcerlean@gmail.com>
2015-02-08 23:20:28 +0000

    Fill chart with cells that can be independently changed

--- chart.js
+++ chart.js
@@ -2,8 +2,23 @@
     var app = angular.module('chart', []);
 
     app.controller('ChartController', function($http) {
-        this.colour = "white";
+        var chartCell = function() {
+            this.colour = "white";
+            this.changeColour = function() {
+                var randColour = Math.floor(Math.random() * 0xFFFFFF);
+                this.colour = "#" + randColour.toString(16);
+            }
+        };
 
+        this.cells = [];
+
+        for (var i = 0; i < 8; i++) {
+            this.cells[i] = [];
+            for (var j = 0; j < 6; j++) {
+                this.cells[i][j] = (new chartCell());
+            }
+        }
+ 
         var self = this;
 
         this.changeColour = function() {
