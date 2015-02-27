afd5000cafeb1661a1cfe07e2bd1aa48900cc717
Bridget McErlean <bridgetmcerlean@gmail.com>
2015-02-27 18:58:46 +0000

    Add a simple colour palette to allow colour selection

--- chart.js
+++ chart.js
@@ -2,28 +2,33 @@
     var app = angular.module('chart', []);
 
     app.controller('ChartController', function($http) {
-        var chartCell = function() {
+        this.CSSformat = function(colour) {
+            return '#' + String('000000' + colour.toString(16)).slice(-6);
+        }
+        
+        var chartCell = function(formatFunction) {
             this.colour = "white";
-            this.changeColour = function() {
-                var randColour = Math.floor(Math.random() * 0xFFFFFF);
-                this.colour = "#" + randColour.toString(16);
+            this.format = formatFunction;
+            this.changeColour = function(colour) {
+                this.colour = this.format(colour);
             }
         };
 
         this.cells = [];
+        this.colour = 0xFFFFFF;
+        this.defaultColours = [0x000000, 0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0x00FFFF, 0xFF00FF, 0xFFFFFF];
+
+        this.selectColour = function(colour) {
+            this.colour = colour; 
+        }
 
         for (var i = 0; i < 8; i++) {
             this.cells[i] = [];
             for (var j = 0; j < 6; j++) {
-                this.cells[i][j] = (new chartCell());
+                this.cells[i][j] = (new chartCell(this.CSSformat));
             }
         }
  
         var self = this;
-
-        this.changeColour = function() {
-            var randColour = Math.floor(Math.random() * 0xFFFFFF);
-            this.colour = "#" + randColour.toString(16);
-        };
     });
 })();
