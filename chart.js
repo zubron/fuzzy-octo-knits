bdadeca8e61046f510757d55f0ddeef5ef4750f6
Bridget McErlean <bridgetmcerlean@gmail.com>
2015-03-01 11:25:33 +0000

    Add functionality to undo/redo clearing of all cells

--- chart.js
+++ chart.js
@@ -2,37 +2,63 @@
     var app = angular.module('chart', []);
 
     app.controller('ChartController', function($http) {
+        var self = this;
+
         var CommandManager = function() {
-            this.undoStack = [];
-            this.redoStack = [];
-            this.addCommand = function(execute, undo) {
-                this.undoStack.push({execute: execute, undo: undo});
-                this.redoStack = [];
-                execute();
-            };
+            var self = this;
+            self.undoStack = [];
+            self.redoStack = [];
 
-            this.undo = function() {
-                var command = this.undoStack.pop();
-                command.undo();
-                this.redoStack.push(command);
-            };
-
-            this.redo = function() {
-                var command = this.redoStack.pop();
+            self.addCommand = function(command) {
+                self.undoStack.push(command);
+                self.redoStack = [];
                 command.execute();
-                this.undoStack.push(command);
             };
 
-            this.canUndo = function() {
-                return this.undoStack.length !== 0;
+            self.addCommandPair = function(execute, undo) {
+                self.addCommand({execute: execute, undo: undo});
             };
 
-            this.canRedo = function() {
-                return this.redoStack.length !== 0;
+            self.undo = function() {
+                var command = self.undoStack.pop();
+                command.undo();
+                self.redoStack.push(command);
+            };
+
+            self.redo = function() {
+                var command = self.redoStack.pop();
+                command.execute();
+                self.undoStack.push(command);
+            };
+
+            self.canUndo = function() {
+                return self.undoStack.length !== 0;
+            };
+
+            self.canRedo = function() {
+                return self.redoStack.length !== 0;
             };
         };
 
         var chartCommandManager = new CommandManager();
+
+        var CompositeCommand = function(executeCommands, undoCommands) {
+            var self = this;
+            self.executeCommands = executeCommands;
+            self.undoCommands = undoCommands;
+
+            self.execute = function() {
+                self.executeCommands.map(function(execute) {
+                    execute();
+                });
+            };
+
+            self.undo = function () {
+                self.undoCommands.map(function(undo) {
+                    undo();
+                });
+            };
+        };
 
         var cssFormat = function(colour) {
             return '#' + String('000000' + colour.toString(16)).slice(-6);
@@ -40,57 +66,69 @@
         
         var ChartCell = function() {
             var self = this;
-            this.colour = "white";
-            this.changeColour = function(colour) {
+            self.colour = cssFormat(0xFFFFFF);
+            self.changeColour = function(colour) {
+                var command = self.changeColourCommand(colour);
+                chartCommandManager.addCommandPair(command.execute, command.undo);
+            };
+
+            self.changeColourCommand = function(colour) {
                 var previousColour = self.colour;
-                chartCommandManager.addCommand(function() {
-                    self.colour = cssFormat(colour);
-                }, function() {
-                    self.colour = previousColour;
+                return {
+                    execute: function() {
+                        self.colour = cssFormat(colour);
+                    },
+                    undo: function() {
+                        self.colour = previousColour;
+                    }
+                };
+            };
+        };
+
+        self.clearCells = function() {
+            var executeCommands = [];
+            var undoCommands = [];
+            self.cells.forEach(function(elementRow) {
+                elementRow.forEach(function(elementCell) {
+                    var command = elementCell.changeColourCommand(0xFFFFFF);
+                    executeCommands.push(command.execute);
+                    undoCommands.push(command.undo);
                 });
-            }
+            });
+            chartCommandManager.addCommand(new CompositeCommand(executeCommands, undoCommands));
+        }
+
+        self.cells = [];
+        self.colour = 0xFFFFFF;
+        self.defaultColours = [0x000000, 0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0x00FFFF, 0xFF00FF, 0xFFFFFF];
+        self.cssFormat = cssFormat;
+
+        self.selectColour = function(colour) {
+            self.colour = colour; 
         };
 
-        this.cells = [];
-        this.colour = 0xFFFFFF;
-        this.defaultColours = [0x000000, 0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0x00FFFF, 0xFF00FF, 0xFFFFFF];
-        this.cssFormat = cssFormat;
-
-        this.selectColour = function(colour) {
-            this.colour = colour; 
-        };
-
-        this.undo = function() {
+        self.undo = function() {
             chartCommandManager.undo();
         };
 
-        this.redo = function() {
+        self.redo = function() {
             chartCommandManager.redo();
         };
 
-        this.canUndo = function() {
+        self.canUndo = function() {
             return chartCommandManager.canUndo();
         };
 
-        this.canRedo = function() {
+        self.canRedo = function() {
             return chartCommandManager.canRedo();
         };
 
         for (var i = 0; i < 8; i++) {
-            this.cells[i] = [];
+            self.cells[i] = [];
             for (var j = 0; j < 6; j++) {
-                this.cells[i][j] = new ChartCell();
+                self.cells[i][j] = new ChartCell();
             }
         }
  
-        this.clearCells = function() {
-            this.cells.forEach(function(elementRow) {
-                elementRow.forEach(function(elementCell) {
-                    elementCell.changeColour(0xFFFFFF);
-                });
-            });
-        }
-
-        var self = this;
-    });
+   });
 })();
